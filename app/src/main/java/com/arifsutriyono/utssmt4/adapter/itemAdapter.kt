b02805736c917192166c1796344b2b8f772fea3b
stryn26 <sutrionoarif@gmail.com>
2022-04-04 16:43:39 +0700

    master

--- app/src/main/java/com/arifsutriyono/utssmt4/adapter/itemAdapter.kt
+++ app/src/main/java/com/arifsutriyono/utssmt4/adapter/itemAdapter.kt
@@ -20,7 +20,7 @@ class itemAdapter (
     //
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView =view.findViewById(R.id.item_title)
-        val imageView: ImageView = view.findViewById(R.id.item_image)
+        val imageView: ImageView =view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -33,6 +33,7 @@ class itemAdapter (
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text =  context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
     override fun getItemCount(): Int {
         return dataset.size
