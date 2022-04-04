4545efd04aa8a41293817494b94b59c6e61bac73
stryn26 <sutrionoarif@gmail.com>
2022-04-04 14:50:50 +0700

    master

--- app/src/main/java/com/arifsutriyono/utssmt4/MainActivity.kt
+++ app/src/main/java/com/arifsutriyono/utssmt4/MainActivity.kt
@@ -2,10 +2,19 @@ package com.arifsutriyono.utssmt4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.arifsutriyono.utssmt4.adapter.itemAdapter
+import com.arifsutriyono.utssmt4.data.dataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDataset = dataSource().loadDataKata()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = itemAdapter(this,myDataset)
+        recyclerView.setHasFixedSize(true)
+
     }
 }
--- app/src/main/java/com/arifsutriyono/utssmt4/adapter/itemAdapter.kt
+++ app/src/main/java/com/arifsutriyono/utssmt4/adapter/itemAdapter.kt
@@ -1,14 +1,39 @@
 package com.arifsutriyono.utssmt4.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.arifsutriyono.utssmt4.R
 import com.arifsutriyono.utssmt4.model.dataKata
 
-class itemAdapter (private val context:Context , private val dataset:List<dataKata>){
+//import library yang dibutuhkan
+
+class itemAdapter (
+    private val context:Context ,
+    private val dataset:List<dataKata>
+    ):RecyclerView.Adapter<itemAdapter.ItemViewHolder>()
+    {
+    //
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView =view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text =  context.resources.getString(item.stringResourceId)
+    }
+    override fun getItemCount(): Int {
+        return dataset.size
     }
 }
