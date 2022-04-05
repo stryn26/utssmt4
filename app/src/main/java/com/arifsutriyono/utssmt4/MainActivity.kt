997d7b05f2cf99b8e5e296f56fc9a659acf1e33a
stryn26 <sutrionoarif@gmail.com>
2022-04-05 11:14:58 +0700

    master

--- app/src/main/java/com/arifsutriyono/utssmt4/MainActivity.kt
+++ app/src/main/java/com/arifsutriyono/utssmt4/MainActivity.kt
@@ -3,7 +3,7 @@ package com.arifsutriyono.utssmt4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
-import com.arifsutriyono.utssmt4.adapter.itemAdapter
+import com.arifsutriyono.utssmt4.adapter.ItemAdapter
 import com.arifsutriyono.utssmt4.data.dataSource
 //import library yang diperlukan
 class MainActivity : AppCompatActivity() {
@@ -13,7 +13,8 @@ class MainActivity : AppCompatActivity() {
 
         val myDataset = dataSource().loadDataKata()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)//memilih id layout recycle view
-        recyclerView.adapter = itemAdapter(this,myDataset)//menggunakan adapte
+
+        recyclerView.adapter = ItemAdapter(this,myDataset)//menggunakan adapter
         recyclerView.setHasFixedSize(true)
 
     }
